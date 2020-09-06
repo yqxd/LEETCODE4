4e9d3804be6dd7e1c078157b948a38075507396f
yqxd <xuz19556@gmail.com>
2020-09-06 10:04:49 +0800

    67

---
+++ 367ValidPerfectSquare.cpp
@@ -0,0 +1,40 @@
+// Given a positive integer num, write a function which returns True if num is a
+// perfect square else False.
+
+// Follow up: Do not use any built-in library function such as sqrt.
+
+// Example 1:
+
+// Input: num = 16
+// Output: true
+// Example 2:
+
+// Input: num = 14
+// Output: false
+
+// Constraints:
+
+// 1 <= num <= 2^31 - 1
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    bool isPerfectSquare(int num) {
+        int result = sqrt(num);
+        return result * result == num;
+    }
+};
+
+int main() {
+    Solution A;
+    cout << A.isPerfectSquare(15) << endl;
+    system("Pause");
+}
